5dd22c5abd8feeb74368a55f164079a2e89ceb81
hdacostac <hdacostac@gmail.com>
2020-10-12 12:50:06 +0200

    working prototype

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ToastService from 'primevue/toastservice';
+
+import en from './translations/en.json';
+import es from './translations/es.json';
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+    locale: 'es',
+    messages: {
+        en,
+        es
+    },
+    fallbackLocale: 'es'
+});
+
+createApp(App).use(store).use(router).use(ToastService).use(i18n).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <Toast />
+    <h2>{{ t('title') }}</h2>
+    <button @click="showToastMessage">Click me</button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -9,10 +12,26 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+import Toast from 'primevue/toast';
+
+import { useI18n } from 'vue-i18n';
+
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Toast
+  },
+  setup() {
+    const {t, locale} = useI18n();
+
+    return {t, locale};
+  },
+  methods: {
+    showToastMessage: function() {
+      console.log("Message showed");
+      this.$toast.add({severity:'success', summary: 'Success Message', detail:'Order submitted', life: 3000});
+    }
   }
 }
 </script>
